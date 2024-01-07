99fe4b8b76e616203dbd58061e8c6e44eb5dde98
badfire <badfire@AMD-AUSTIN>
2024-01-07 14:20:36 -0600

    asdf

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,6 @@
+import openai
 
+openai.api_key "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"
+
+completion = openai.ChatCompletion.create (model="gpt-3.5-turbo", messages=[{"role": "user": "write an essay about peguins"}])
+print(completion.choices[0].message.content)
